0774f734fe61b0024484c6bc21281ea7c744ad58
Joe Colley <josephthco@gmail.com>
2023-05-24 10:52:01 -0400

    File Uploading and XML reading

--- Project 1/main.js
+++ Project 1/main.js
@@ -22,14 +22,33 @@ function main()
 
     //get the input and add a listener for file upload
     const fileUpload = document.getElementById("files");
+    fileUpload.type = "file";
     fileUpload.addEventListener("change",on_file_upload,false);
 
-    function on_file_upload()
+    function on_file_upload(event)
     {
         //get the uploaded file
-        const file = this.files[0];
+        const file = event.target.files[0];
         console.log(file.name + " uploaded!");
 
+        //open the file as xml
+        const reader = new FileReader();
+        reader.onload = function (e){
+            //get file contents
+            const fileContent = e.target.result;
+
+            //make file parser and read file as XML
+            const parser = new DOMParser();
+            const xml_Doc = parser.parseFromString(fileContent.toString(),"application/xml");
+
+            //using supplied methods, get the viewbox and line info
+            const view_box = xmlGetViewbox(xml_Doc,400);
+            const lines = xmlGetLines(xml_Doc,0x000000);
+
+            console.log(view_box)
+            console.log(lines)
+        }
+        reader.readAsText(file);
     }
 }
 
