91adcbb67846965853a2677b3dac065201538987
Joe Colley <josephthco@gmail.com>
2023-06-10 17:45:02 -0400

    Lighting, Removed HTML, Changed for requirements

--- Project 1/Part 2/main.js
+++ Project 1/Part 2/main.js
@@ -2,13 +2,13 @@ let canvas;
 let gl;
 let program;
 
-let sphere_subdivisions = 2;
 const MIN_SPHERE_SUBDIVISIONS = 0;
-const MAX_SPHERE_SUBDIVISIONS = 4;
+const MAX_SPHERE_SUBDIVISIONS = 5;
+let sphere_subdivisions = 0;
 
-let line_subdivisions = 3;
-const MIN_LINE_SUBDIVISIONS = 1;
-const MAX_LINE_SUBDIVISIONS = 5;
+const MIN_LINE_SUBDIVISIONS = 0;
+const MAX_LINE_SUBDIVISIONS = 8;
+let line_subdivisions = 2;
 
 let line_points = [];
 let line_control_points = []
@@ -17,16 +17,13 @@ let position_attribute_location;
 
 let is_playing = true;
 let t = 0;
-let t_speed = 0.005;
-
-let sphere_points = [];
-let sphere_normals = [];
-let sphere_flat_shading = [];
+let t_speed = 0;
+const MAX_SPEED = 0.005;
 
 let lightPosition = vec4(5.0, 5.0, -5.0, 0.0);
-let lightAmbient = vec4(1.0, 1.0, 1.0, 0.5);
+let lightAmbient = vec4(1.0, 1.0, 1.0, 1.0);
 let lightDiffuse = vec4(1.0, 1.0, 1.0, 1.0);
-let lightSpecular = vec4(0.5, 0.5, 0.5, 1.0);
+let lightSpecular = vec4(1.0, 1.0, 1.0, 1.0);
 
 let materialAmbient = vec4(1.0, 0.0, 0.0, 1.0);
 let materialDiffuse = vec4(1.0, 1.0, 1.0, 1.0);
@@ -36,13 +33,32 @@ let materialShininess = 20;
 let modelViewMatrix, projectionMatrix;
 let modelViewMatrixLoc, projectionMatrixLoc;
 
-let sphere_vBuffer, sphere_vNormalBuffer, chaikin_vBuffer;
+let sphere_vBuffer, sphere_normal_buffer, chaikin_vBuffer;
 let line_start_location, line_end_location, progress_location;
 
 let fovy = 105;
 let eye = vec3(0.0, 0.0, 5.0);
 let at = vec3(0.0, 0.0, 0.0);
 let up = vec3(0.0, 1.0, 0.0);
+
+const initial_vertices = [
+    -0.5, -0.5, -0.5,
+    0.5, -0.5, -0.5,
+    0.5, 0.5, -0.5,
+    -0.5, 0.5, -0.5,
+    -0.5, -0.5, 0.5,
+    0.5, -0.5, 0.5,
+    0.5, 0.5, 0.5,
+    -0.5, 0.5, 0.5
+];
+const initial_indices = [
+    0, 1, 2, 0, 2, 3,  // Front face
+    1, 5, 6, 1, 6, 2,  // Right face
+    5, 4, 7, 5, 7, 6,  // Back face
+    4, 0, 3, 4, 3, 7,  // Left face
+    3, 2, 6, 3, 6, 7,  // Top face
+    4, 5, 1, 4, 1, 0   // Bottom face
+];
 
 function clamp(value, min, max) {
     return Math.min(Math.max(value, min), max);
@@ -85,10 +101,13 @@ function init() {
     }
 
     window.addEventListener('keydown', on_key_down);
+    window.addEventListener('keyup', on_key_up);
 
     gl.viewport(0, 0, canvas.width, canvas.height);
     gl.clearColor(0.0, 0.0, 0.0, 1.0);
     gl.enable(gl.DEPTH_TEST);
+
+    console.log(line_subdivisions + " " + sphere_subdivisions);
 
     //
     //  Load shaders and initialize attribute buffers
@@ -99,9 +118,6 @@ function init() {
     line_start_location = gl.getUniformLocation(program, "u_line_start");
     line_end_location = gl.getUniformLocation(program, "u_line_end");
     progress_location = gl.getUniformLocation(program, "u_progress");
-
-    set_color();
-    set_speed();
 
     view_init();
     lighting_init();
@@ -133,7 +149,7 @@ function lighting_init() {
 
 function sphere_init() {
     sphere_vBuffer = gl.createBuffer();
-    sphere_vNormalBuffer = gl.createBuffer();
+    sphere_normal_buffer = gl.createBuffer();
 }
 
 function chaikin_init() {
@@ -159,32 +175,12 @@ function render() {
     requestAnimationFrame(render);
 }
 
-const initial_vertices = [
-    // Cube vertices
-    -0.5, -0.5, -0.5,
-    0.5, -0.5, -0.5,
-    0.5, 0.5, -0.5,
-    -0.5, 0.5, -0.5,
-    -0.5, -0.5, 0.5,
-    0.5, -0.5, 0.5,
-    0.5, 0.5, 0.5,
-    -0.5, 0.5, 0.5
-];
-
-const initial_indices = [
-    // Cube indices
-    0, 1, 2, 0, 2, 3,  // Front face
-    1, 5, 6, 1, 6, 2,  // Right face
-    5, 4, 7, 5, 7, 6,  // Back face
-    4, 0, 3, 4, 3, 7,  // Left face
-    3, 2, 6, 3, 6, 7,  // Top face
-    4, 5, 1, 4, 1, 0   // Bottom face
-];
-
 function subdivide_cube(sub_count) {
     const vertices = initial_vertices.slice();
     let indices = initial_indices.slice();
+    let normals = [];
 
+    //subdivide
     for (let level = 0; level < sub_count; level++) {
         const newVertices = [];
         const newIndices = [];
@@ -232,6 +228,7 @@ function subdivide_cube(sub_count) {
         indices = newIndices;
     }
 
+    //normalize
     for (let i = 0; i < vertices.length; i += 3) {
         const vertex = vertices.slice(i, i + 3);
         const length = Math.sqrt(vertex[0] ** 2 + vertex[1] ** 2 + vertex[2] ** 2);
@@ -240,7 +237,38 @@ function subdivide_cube(sub_count) {
         vertices[i + 2] /= length;
     }
 
-    return {sub_vertices: vertices, sub_indices: indices}
+    //face normals
+    for (let i = 0; i < indices.length; i += 3) {
+        const v1 = indices[i] * 3;
+        const v2 = indices[i + 1] * 3;
+        const v3 = indices[i + 2] * 3;
+
+        const p1 = vertices.slice(v1, v1 + 3);
+        const p2 = vertices.slice(v2, v2 + 3);
+        const p3 = vertices.slice(v3, v3 + 3);
+
+        const u = [p2[0] - p1[0], p2[1] - p1[1], p2[2] - p1[2]];
+        const v = [p3[0] - p1[0], p3[1] - p1[1], p3[2] - p1[2]];
+
+        const normal = [
+            u[1] * v[2] - u[2] * v[1],
+            u[2] * v[0] - u[0] * v[2],
+            u[0] * v[1] - u[1] * v[0]
+        ];
+
+        const length = Math.sqrt(normal[0] ** 2 + normal[1] ** 2 + normal[2] ** 2);
+        normal[0] /= length;
+        normal[1] /= length;
+        normal[2] /= length;
+
+        normals.push(...normal, ...normal, ...normal);
+    }
+
+    return {
+        sub_vertices: vertices,
+        sub_indices: indices,
+        sub_normals: normals
+    }
 }
 
 function make_sphere(sphere_info) {
@@ -258,8 +286,12 @@ function make_sphere(sphere_info) {
     gl.enableVertexAttribArray(position_attribute_location);
     gl.vertexAttribPointer(position_attribute_location, 3, gl.FLOAT, false, 0, 0);
 
-    gl.bindBuffer(gl.ARRAY_BUFFER, sphere_vNormalBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, flatten(sphere_normals), gl.STATIC_DRAW);
+    gl.bindBuffer(gl.ARRAY_BUFFER, sphere_normal_buffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(sphere_info.sub_normals), gl.STATIC_DRAW);
+
+    let normal_attribute_location = gl.getAttribLocation(program, "a_Normal");
+    gl.enableVertexAttribArray(normal_attribute_location);
+    gl.vertexAttribPointer(normal_attribute_location, 4, gl.FLOAT, false, 0, 0);
 }
 
 function render_sphere(sphere_info) {
@@ -279,14 +311,14 @@ function make_chaikin() {
     let size = 5.0;
 
     //top arc
-    line_control_points.push(vec4(size, 0.0, 0.0, 1.0));
-    line_control_points.push(vec4(0.0, size, 0.0, 1.0));
-    line_control_points.push(vec4(-size, 0.0, 0.0, 1.0));
+    line_control_points.push(vec4(size, 0.0, 0.0));
+    line_control_points.push(vec4(0.0, size, 0.0));
+    line_control_points.push(vec4(-size, 0.0, 0.0));
 
     //bot arc
-    line_control_points.push(vec4(size, 0.0, 0.0, 1.0));
-    line_control_points.push(vec4(0.0, -size, 0.0, 1.0));
-    line_control_points.push(vec4(-size, 0.0, 0.0, 1.0));
+    line_control_points.push(vec4(size, 0.0, 0.0));
+    line_control_points.push(vec4(0.0, -size, 0.0));
+    line_control_points.push(vec4(-size, 0.0, 0.0));
 
     line_points = chaikin(line_control_points, line_subdivisions);
 }
@@ -307,11 +339,13 @@ function render_chaikin() {
     gl.vertexAttribPointer(position_attribute_location, 4, gl.FLOAT, false, 0, 0);
     gl.enableVertexAttribArray(position_attribute_location);
 
-    gl.drawArrays(gl.LINES, 0, line_points.length);
+    gl.drawArrays(gl.LINE_STRIP, 0, line_points.length);
 }
 
 function update_sphere_position() {
     if (is_playing) {
+        t += t_speed;
+
         if (t < 1) {
             const t_index = Math.floor(t * (line_points.length - 1));
             const progress = (t * (line_points.length - 1)) % 1;
@@ -323,14 +357,16 @@ function update_sphere_position() {
 
             gl.uniform3fv(line_start_location, [start[0], start[1], start[2]]);
             gl.uniform3fv(line_end_location, [end[0], end[1], end[2]]);
-
-            t += t_speed;
-            if (t > 1) {
-                t = 0;
-            }
         } else {
             t = 0;
         }
+    }
+}
+
+function on_key_up(event) {
+    const key = event.key;
+    if (key === 'a') {
+        t_speed = 0;
     }
 }
 
@@ -344,28 +380,9 @@ function on_key_down(event) {
         line_subdivision_up();
     } else if (key === 'j') {
         line_subdivision_down();
+    } else if (key === 'a') {
+        t_speed = MAX_SPEED;
     }
-}
-
-function set_speed() {
-    const speed_slider = document.getElementById("speed_slider");
-    t_speed = parseFloat(speed_slider.value);
-}
-
-function set_color() {
-    const red_slider = document.getElementById("red_slider");
-    const green_slider = document.getElementById("green_slider");
-    const blue_slider = document.getElementById("blue_slider");
-
-    const r = parseFloat(red_slider.value);
-    const g = parseFloat(green_slider.value);
-    const b = parseFloat(blue_slider.value);
-
-    materialAmbient = vec4(r, g, b, 1.0);
-}
-
-function on_paused_checked() {
-    is_playing = !is_playing;
 }
 
 function clamp_line_subdivisions() {
