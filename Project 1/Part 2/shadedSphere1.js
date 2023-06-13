776484bd47c6d33536f5526e4c0240634799cf6e
Joe Colley <josephthco@gmail.com>
2023-06-13 16:21:48 -0400

    2.1, 2.2, and 2.4 good progress

--- Project 1/Part 2/shadedSphere1.js
+++ Project 1/Part 2/shadedSphere1.js
@@ -2,40 +2,38 @@ let canvas;
 let gl;
 let program;
 
-let numTimesToSubdivide = 5;
+let sphere_subdivsions = 1;
+const MAX_SPHERE_SUB = 5;
+const MIN_SPHERE_SUB = 0;
 
 let index = 0;
 
 let pointsArray = [];
 let normalsArray = [];
 
-let near = -10;
-let far = 10;
-
-let left = -3.0;
-let right = 3.0;
-let ytop = 3.0;
-let bottom = -3.0;
+let fovy = 90;
+let near = -1;
+let far = 1;
 
 let va = vec4(0.0, 0.0, -1.0, 1);
 let vb = vec4(0.0, 0.942809, 0.333333, 1);
 let vc = vec4(-0.816497, -0.471405, 0.333333, 1);
 let vd = vec4(0.816497, -0.471405, 0.333333, 1);
 
-let lightPosition = vec4(1.0, 1.0, 1.0, 0.0);
-let lightAmbient = vec4(0.2, 0.2, 0.2, 1.0);
+let lightPosition = vec4(-5.0, -5.0, -5.0, 0.0);
+let lightAmbient = vec4(1.5, 1.0, 0.0, 1.0);
 let lightDiffuse = vec4(1.0, 1.0, 1.0, 1.0);
 let lightSpecular = vec4(1.0, 1.0, 1.0, 1.0);
 
-let materialAmbient = vec4(1.0, 0.0, 1.0, 1.0);
-let materialDiffuse = vec4(1.0, 0.8, 0.0, 1.0);
-let materialSpecular = vec4(1.0, 1.0, 1.0, 1.0);
-let materialShininess = 20.0;
+let materialAmbient = vec4(1.0, 1.0, 1.0, 1.0);
+let materialDiffuse = vec4(1.0, 1.0, 1.0, 1.0);
+let materialSpecular = vec4(2.5, 2.5, 2.5, 1.0);
+let materialShininess = 100.0;
 
 let modelViewMatrix, projectionMatrix;
 let modelViewMatrixLoc, projectionMatrixLoc;
 
-let eye = vec3(0, 0, 1.5);
+let eye = vec3(0, 0, 5.0);
 let at = vec3(0.0, 0.0, 0.0);
 let up = vec3(0.0, 1.0, 0.0);
 
@@ -53,6 +51,7 @@ function triangle(a, b, c) {
     index += 3;
 
 }
+
 function divideTriangle(a, b, c, count) {
     if (count > 0) {
         let ab = mix(a, b, 0.5);
@@ -71,6 +70,7 @@ function divideTriangle(a, b, c, count) {
         triangle(a, b, c);
     }
 }
+
 function tetrahedron(a, b, c, d, n) {
     divideTriangle(a, b, c, n);
     divideTriangle(d, c, b, n);
@@ -79,7 +79,6 @@ function tetrahedron(a, b, c, d, n) {
 }
 
 function init() {
-
     canvas = document.getElementById("gl-canvas");
 
     gl = WebGLUtils.setupWebGL(canvas);
@@ -88,8 +87,7 @@ function init() {
     }
 
     gl.viewport(0, 0, canvas.width, canvas.height);
-    gl.clearColor(1.0, 1.0, 1.0, 1.0);
-
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
     gl.enable(gl.DEPTH_TEST);
 
     //
@@ -98,12 +96,20 @@ function init() {
     program = initShaders(gl, "vertex-shader", "fragment-shader");
     gl.useProgram(program);
 
+    index = 0;
+    pointsArray = [];
+    normalsArray = [];
+
+    window.addEventListener("keydown", on_key_down);
+
+    //lighting
     let diffuseProduct = mult(lightDiffuse, materialDiffuse);
     let specularProduct = mult(lightSpecular, materialSpecular);
     let ambientProduct = mult(lightAmbient, materialAmbient);
 
-    tetrahedron(va, vb, vc, vd, numTimesToSubdivide);
+    tetrahedron(va, vb, vc, vd, sphere_subdivsions);
 
+    //GPU info
     let vBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
     gl.bufferData(gl.ARRAY_BUFFER, flatten(pointsArray), gl.STATIC_DRAW);
@@ -120,9 +126,11 @@ function init() {
     gl.vertexAttribPointer(vNormalPosition, 4, gl.FLOAT, false, 0, 0);
     gl.enableVertexAttribArray(vNormalPosition);
 
+    //model info
     modelViewMatrixLoc = gl.getUniformLocation(program, "modelViewMatrix");
     projectionMatrixLoc = gl.getUniformLocation(program, "projectionMatrix");
 
+    //push lighting
     gl.uniform4fv(gl.getUniformLocation(program, "diffuseProduct"), flatten(diffuseProduct));
     gl.uniform4fv(gl.getUniformLocation(program, "specularProduct"), flatten(specularProduct));
     gl.uniform4fv(gl.getUniformLocation(program, "ambientProduct"), flatten(ambientProduct));
@@ -136,7 +144,9 @@ function render() {
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
     modelViewMatrix = lookAt(eye, at, up);
-    projectionMatrix = ortho(left, right, bottom, ytop, near, far);
+    //projectionMatrix = ortho(left, right, bottom, ytop, near, far);
+
+    projectionMatrix = perspective(fovy, 1, near, far);
 
     gl.uniformMatrix4fv(modelViewMatrixLoc, false, flatten(modelViewMatrix));
     gl.uniformMatrix4fv(projectionMatrixLoc, false, flatten(projectionMatrix));
@@ -144,4 +154,29 @@ function render() {
     for (let i = 0; i < index; i += 3) {
         gl.drawArrays(gl.TRIANGLES, i, 3);
     }
+}
+
+function on_key_down(event) {
+    const key = event.key;
+
+    if (key === 'q') {
+        on_sphere_subdivision_change(-1);
+    } else if (key === 'e') {
+        on_sphere_subdivision_change(1);
+    }
+}
+
+function on_sphere_subdivision_change(change) {
+    sphere_subdivsions += change;
+
+    //clamp?
+    if (sphere_subdivsions < MIN_SPHERE_SUB) {
+        sphere_subdivsions = MIN_SPHERE_SUB;
+    }
+    if (sphere_subdivsions > MAX_SPHERE_SUB) {
+        sphere_subdivsions = MAX_SPHERE_SUB;
+    }
+
+    console.log(sphere_subdivsions);
+    init();
 }
